187313d1d9cb38781d28c9ada74cb96de0bce680
Tomasz Letkiewicz <tk.letkiewicz@gmail.com>
2022-02-01 15:12:51 +0100

    Methods for depart time and date. Methods for return time and date. Perform Search method.

--- src/test/java/pl/testeroprogramowania/pages/CarSearchPage.java
+++ src/test/java/pl/testeroprogramowania/pages/CarSearchPage.java
@@ -1,4 +1,73 @@
 package pl.testeroprogramowania.pages;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class CarSearchPage {
+    @FindBy(xpath = "//span[contains(text(), 'Cars')]")
+    private WebElement carSearchButton;
+
+    @FindBy(xpath = "//span[contains(text(), 'Pick up')]")
+    private WebElement searchCarSpan;
+
+    @FindBy(xpath = "//div[@id='select2-drop']//input")
+    private WebElement locationInput;
+
+    @FindBy(id = "departcar")
+    private WebElement carDepart;
+
+    @FindBy(name = "pickupTime")
+    private WebElement pickUpTime;
+
+    @FindBy(id = "returncar")
+    private WebElement carReturn;
+
+    @FindBy(name = "dropoffTime")
+    private WebElement dropOffTime;
+
+    @FindBy(xpath = "//*[@id='cars']/form/div[7]/button")
+    private WebElement pressCarSearchButton;
+
+
+    private WebDriver driver;
+
+    public void clickCarsButton(WebDriver driver){
+        carSearchButton.click();
+    }
+    public void setLocation(String cityName) {
+        searchCarSpan.click();
+        locationInput.sendKeys(cityName);
+        locationInput.sendKeys(Keys.ENTER);
+
+    }
+    public void setDepDate(String depDate){
+        carDepart.clear();
+        carDepart.sendKeys(depDate);
+    }
+    public void setDepTime(){
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].value='03:30';", pickUpTime);
+    }
+    public void setReturnDate(String returnDate){
+        carReturn.clear();
+        carReturn.sendKeys(returnDate);
+    }
+    public void setReturnTime(){
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("arguments[0].value='08:30';", dropOffTime);
+    }
+    public void performSearch(){
+        pressCarSearchButton.click();
+    }
+
+    public CarSearchPage(WebDriver driver){
+        PageFactory.initElements(driver,this);
+        this.driver=driver;
+    }
+
+
 }
--- src/test/java/pl/testeroprogramowania/tests/CarSearchTest.java
+++ src/test/java/pl/testeroprogramowania/tests/CarSearchTest.java
@@ -1,4 +1,21 @@
 package pl.testeroprogramowania.tests;
 
-public class CarSearchTest {
+import org.testng.annotations.Test;
+import pl.testeroprogramowania.pages.CarSearchPage;
+
+public class CarSearchTest extends BaseTest {
+
+    @Test
+    public void searchForCarToRent() {
+        CarSearchPage carSearchPage = new CarSearchPage(driver);
+        carSearchPage.clickCarsButton(driver);
+        carSearchPage.setLocation("Manchester");
+        carSearchPage.setDepDate("12/02/2022");
+        carSearchPage.setDepTime();
+        carSearchPage.setReturnDate("16/02/2022");
+        carSearchPage.setReturnTime();
+        carSearchPage.performSearch();
+    }
+
+
 }
