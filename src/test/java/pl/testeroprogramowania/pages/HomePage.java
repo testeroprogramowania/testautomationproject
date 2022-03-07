151707619d0d44c4408facfc0a391942f973184a
Tomasz Letkiewicz <tk.letkiewicz@gmail.com>
2022-03-07 12:07:44 +0100

    Added: SignUpPage with constructor

--- src/test/java/pl/testeroprogramowania/pages/HomePage.java
+++ src/test/java/pl/testeroprogramowania/pages/HomePage.java
@@ -5,10 +5,18 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class HomePage {
 
     @FindBy(xpath = "//span[contains(text(), 'Flights')]")
     private WebElement searchFlightButton;
+
+    @FindBy(xpath = "//li[@id='li_myaccount']")
+    private List<WebElement> myAccountButtonLink;
+
+    @FindBy(xpath = "//a[text()='  Sign Up']")
+    private List<WebElement> signUpLink;
 
 
     private WebDriver driver;
@@ -22,6 +30,12 @@ public FlightSearchPage openFlightSearchPage(){
         searchFlightButton.click();
         return new FlightSearchPage(driver);
 }
+    public SignUpPage openSignUpForm() {
+        myAccountButtonLink.stream().filter(WebElement::isDisplayed)
+                .findFirst().ifPresent(WebElement::click);
+        signUpLink.get(1).click();
+        return new SignUpPage(driver);
+    }
 
 
 }
