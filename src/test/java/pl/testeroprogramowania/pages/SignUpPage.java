d84aa82174d93954dc805af3246e895af2aac1c3
Tomasz Letkiewicz <tk.letkiewicz@gmail.com>
2022-03-09 09:17:58 +0100

    Added: New user using the Ukrainian alphabet.

--- src/test/java/pl/testeroprogramowania/pages/SignUpPage.java
+++ src/test/java/pl/testeroprogramowania/pages/SignUpPage.java
@@ -4,7 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
 import pl.testeroprogramowania.models.Customer;
+
+import java.util.List;
 
 public class SignUpPage {
     @FindBy(name = "firstname")
@@ -28,6 +31,9 @@ public class SignUpPage {
     @FindBy(xpath = "//button[text()=' Sign Up']")
     private WebElement signUpButton;
 
+    @FindBy(xpath = "//div[@class='alert alert-danger']")
+    private WebElement signUpErrors;
+
     private WebDriver driver;
 
     public SignUpPage(WebDriver driver) {
@@ -44,8 +50,25 @@ public class SignUpPage {
         confirmPasswordInput.sendKeys(customer.getPassword());
         signUpButton.click();
 
-
         return new LoggedUserPage(driver);
+
     }
 
+    public SignUpPage fillTheFormWithExistentEmail(Customer customer) {
+        firstNameInput.sendKeys(customer.getFirstName());
+        lastNameInput.sendKeys(customer.getLastName());
+        phoneInput.sendKeys(customer.getPhone());
+        emailInput.sendKeys(customer.getEmail());
+        passwordInput.sendKeys(customer.getPassword());
+        confirmPasswordInput.sendKeys(customer.getPassword());
+        signUpButton.click();
+
+        return this;
+
+    }
+
+
+    public WebElement getSignUpErrors() {
+        return signUpErrors;
+    }
 }
